be0fd0eb258f9276eda5701506c5db3c17c26e71
Shakira <ndagire.shakira@gmail.com>
2025-02-18 10:44:01 +0300

    Fix category id

--- e2e-testing/cypress/e2e/5-categories/category.cy.js
+++ e2e-testing/cypress/e2e/5-categories/category.cy.js
@@ -27,7 +27,7 @@ describe('Automated Tests for Categories', () => {
 
   it('Deletes a parent category - Bulk Actions', () => {
     categoryFunctions.open_category_list_page();
-    categoryFunctions.delete_category_bulk_actions('#mat-checkbox-59');
+    categoryFunctions.delete_category_bulk_actions('#mat-checkbox-12');
     categoryFunctions.verify_parent_category_deleted();
   });
 
